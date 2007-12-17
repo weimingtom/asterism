71a4a4ee8a5dc49a4a24c051b93c9d10b8ef0c31
hajimehoshi <hajimehoshi@135c6c2e-4e41-0410-bceb-d18500efb546>
2007-12-17 14:55:42 +0000

    Modified main.rb

--- trunk/main.rb
+++ trunk/main.rb
@@ -1,6 +1,5 @@
 require "wx"
 require "starruby"
-require "texture_panel"
 
 module Wx
   class DC
@@ -13,70 +12,19 @@ module Wx
   end
 end
 
+require "item_editor_panel"
+
 module Asterism
-
   class MainFrame < Wx::Frame
-
-    def initialize
-      super(nil, :title => "Asterism", :size => [800, 600])
-
-      main_menu = Wx::MenuBar.new
-      
-      menu = Wx::Menu.new
-      menu.append(Wx::ID_EXIT, "E&xit")
-      evt_menu(Wx::ID_EXIT) {exit}
-      main_menu.append(menu, "&File")
-      
-      menu = Wx::Menu.new
-      menu.append(Wx::ID_HIGHEST + 1, "&Run")
-      evt_menu(Wx::ID_HIGHEST + 1) do
-        begin
-          disable
-          StarRuby::Game.run(320, 240) do
-          end
-        ensure
-          enable
-          set_focus
-        end
-      end
-      main_menu.append(menu, "&Game")
-
-      menu = Wx::Menu.new
-      menu.append(Wx::ID_ABOUT, "&About")
-      main_menu.append(menu, "&Help")
-      self.menu_bar = main_menu
-      
-      status_bar = Wx::StatusBar.new(self)
-      self.status_bar = status_bar
-      
-      main_splitter_class = Class.new(Wx::SplitterWindow)
-      main_splitter_class.class_eval %Q{
-        def initialize(parent)
-          super(parent, object_id)
-          evt_splitter_sash_pos_changed(object_id) do |e|
-            e.sash_position = self.sash_position = 200
-          end
-          evt_splitter_sash_pos_changing(object_id) do |e|
-            e.sash_position = self.sash_position = 200
-          end
-        end
-      }
-      splitter = main_splitter_class.new(self)
-      
-      splitter2 = Wx::SplitterWindow.new(splitter)
-      panel = Wx::TextCtrl.new(splitter2)
-      tree = Wx::TextCtrl.new(splitter2)
-      splitter2.split_horizontally(panel, tree, 500)
-      splitter2.minimum_pane_size = 20
-      
-      splitter.split_vertically(splitter2, TexturePanel.new(splitter, 1000), 200)
-      splitter.minimum_pane_size = 20
+    def initialize(title)
+      super(nil, :title => title)
+      center(Wx::BOTH)
+      ItemEditorPanel.new(self)
     end
   end
-  
 end
 
 Wx::App.run do
-  self.app_name = "Asterism"
-  Asterism::MainFrame.new.show
+  x = Asterism::MainFrame.new("Asterism")
+  x.show
 end
---
+++ trunk/map_editor_frame.rb
@@ -0,0 +1,62 @@
+module Asterism
+
+  class MapEditorFrame < Wx::Frame
+
+    def initialize
+      super(nil, :title => "Asterism", :size => [800, 600])
+
+      main_menu = Wx::MenuBar.new
+      
+      menu = Wx::Menu.new
+      menu.append(Wx::ID_EXIT, "E&xit")
+      evt_menu(Wx::ID_EXIT) {exit}
+      main_menu.append(menu, "&File")
+      
+      menu = Wx::Menu.new
+      menu.append(Wx::ID_HIGHEST + 1, "&Run")
+      evt_menu(Wx::ID_HIGHEST + 1) do
+        begin
+          disable
+          StarRuby::Game.run(320, 240) do
+          end
+        ensure
+          enable
+          set_focus
+        end
+      end
+      main_menu.append(menu, "&Game")
+
+      menu = Wx::Menu.new
+      menu.append(Wx::ID_ABOUT, "&About")
+      main_menu.append(menu, "&Help")
+      self.menu_bar = main_menu
+      
+      status_bar = Wx::StatusBar.new(self)
+      self.status_bar = status_bar
+      
+      main_splitter_class = Class.new(Wx::SplitterWindow)
+      main_splitter_class.class_eval %Q{
+        def initialize(parent)
+          super(parent, object_id)
+          evt_splitter_sash_pos_changed(object_id) do |e|
+            e.sash_position = self.sash_position = 200
+          end
+          evt_splitter_sash_pos_changing(object_id) do |e|
+            e.sash_position = self.sash_position = 200
+          end
+        end
+      }
+      splitter = main_splitter_class.new(self)
+      
+      splitter2 = Wx::SplitterWindow.new(splitter)
+      panel = Wx::TextCtrl.new(splitter2)
+      tree = Wx::TextCtrl.new(splitter2)
+      splitter2.split_horizontally(panel, tree, 500)
+      splitter2.minimum_pane_size = 20
+      
+      splitter.split_vertically(splitter2, TexturePanel.new(splitter, 1000), 200)
+      splitter.minimum_pane_size = 20
+    end
+  end
+  
+end
