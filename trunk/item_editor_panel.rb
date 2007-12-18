86b445dac18068346212670f88b30dbb50cc9171
hajimehoshi <hajimehoshi@135c6c2e-4e41-0410-bceb-d18500efb546>
2007-12-18 13:12:17 +0000

    Modified

--- trunk/item_editor_panel.rb
+++ trunk/item_editor_panel.rb
@@ -4,17 +4,30 @@ module Asterism
   
   class ItemEditorPanel < Wx::Panel
     
-    def initialize(parent)
+    attr_reader :data
+    
+    def initialize(parent, data)
       super(parent, -1)
-      l1 = Wx::StaticText.new(self, -1, "Name:")
-      t1 = Wx::TextCtrl.new(self, -1)
-      l2 = Wx::StaticText.new(self, -1, "Cost:")
-      t2 = Wx::TextCtrl.new(self, -1)
+      @data = data
       
-      sizer = Wx::FlexGridSizer.new(0, 2, 8, 8)
+      id = Wx::ID_HIGHEST
+      l1 = Wx::StaticText.new(self, -1, "Name:")
+      t1 = Wx::TextCtrl.new(self, (id += 1))
+      t1.value = data[:name] if data.include?(:name)
+      evt_text(t1.id) do |e|
+        @data[:name] = e.string
+      end
+      
+      l2 = Wx::StaticText.new(self, -1, "Cost:")
+      t2 = Wx::TextCtrl.new(self, (id += 1))
+      t2.value = data[:cost] if data.include?(:cost)
+      evt_text(t2.id) do |e|
+        @data[:cost] = e.string
+      end
+      
+      sizer = Wx::FlexGridSizer.new(0, 2, 10, 10)
       sizer.add(l1)
       sizer.add(t1)
-      sizer.add(0, 0)
       sizer.add(l2)
       sizer.add(t2)
       
--- trunk/main.rb
+++ trunk/main.rb
@@ -1,5 +1,4 @@
 require "wx"
-require "starruby"
 
 module Wx
   class DC
@@ -19,12 +18,62 @@ module Asterism
     def initialize(title)
       super(nil, :title => title)
       center(Wx::BOTH)
-      ItemEditorPanel.new(self)
+      
+      panel_sizer = Wx::BoxSizer.new(Wx::HORIZONTAL)
+      panel_sizer.add((panel = Wx::Panel.new(self)), 1, Wx::EXPAND | Wx::ALL)
+      self.sizer = panel_sizer
+      
+      sizer = Wx::BoxSizer.new(Wx::VERTICAL)
+      data = {}
+      if File.file?("data")
+        open("data", "r") do |fp|
+          data = eval(fp.read)
+        end
+      end
+      sizer.add(ItemEditorPanel.new(panel, data), 1, Wx::ALIGN_TOP | Wx::ALL)
+      
+      id = Wx::ID_HIGHEST
+      buttons_sizer = Wx::BoxSizer.new(Wx::HORIZONTAL)
+      b = Wx::Button.new(panel, (id += 1), "OK", :size => [100, 30])
+      evt_button(b.id) do |e|
+        open("data", "w") do |fp|
+          fp.puts("{")
+          begin
+            data.each do |key, value|
+              case value
+              when String
+                fp.puts("  #{key.inspect} => #{value.dump},")
+              when Symbol
+                fp.puts("  #{key.inspect} => #{value.inspect},")
+              when Integer
+                fp.puts("  #{key.inspect} => #{value},")
+              end
+            end
+          ensure
+            fp.puts("}")
+          end
+          close
+        end
+      end
+      buttons_sizer.add(b, 0)
+      buttons_sizer.add(10, 0)
+      b = Wx::Button.new(panel, (id += 1), "Cancel", :size => [100, 30])
+      evt_button(b.id) do |e|
+        close
+      end
+      buttons_sizer.add(b, 0)
+      sizer.add(buttons_sizer, 0, Wx::ALIGN_BOTTOM | Wx::ALIGN_RIGHT)
+      
+      panel.sizer = Wx::BoxSizer.new(Wx::HORIZONTAL)
+      panel.sizer.add(sizer, 1, Wx::EXPAND | Wx::ALL, 10)
+    end
+  end
+  
+  class MainApp < Wx::App
+    def on_init
+      MainFrame.new("Asterism").show
     end
   end
 end
 
-Wx::App.run do
-  x = Asterism::MainFrame.new("Asterism")
-  x.show
-end
+Asterism::MainApp.new.main_loop
