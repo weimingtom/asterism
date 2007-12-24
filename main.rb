e46ee434fae23cb546189c402bafdf1915de0e30
hajimehoshi <hajimehoshi@135c6c2e-4e41-0410-bceb-d18500efb546>
2007-12-24 13:39:48 +0000

    Added controller.rb

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 require "wx"
 require "starruby"
 require "yaml"
+require "yaml/store"
 
 module Wx
   class DC
@@ -14,31 +15,33 @@ module Wx
 end
 
 require "model_editor"
+require "controller"
 
 module Asterism
   
   class ModelSelector < Wx::TreeCtrl
     
-    def initialize(parent, editor_panel, models)
+    def initialize(parent, editor_panel, model_schemas)
       super(parent, :style => Wx::TR_SINGLE | Wx::TR_DEFAULT_STYLE)
       root = add_root("Models")
       select_item(root)
       nodes   = {}
       editors = {}
-      models.each do |model|
-        nodes[model] = append_item(root, model[:name])
-        editor = editors[model] = ModelEditor.new(editor_panel, model, [])
-        editor_panel.sizer.add(editor, 0, Wx::EXPAND | Wx::ALL)
+      model_schemas.each do |schema|
+        name = schema[:name]
+        nodes[name] = append_item(root, name)
+        controller = Controller.new(YAML::Store.new("data/#{name}.yaml"))
+        editor = editors[name] = ModelEditor.new(editor_panel, controller)
+        editor_panel.sizer.add(editor, 1, Wx::EXPAND)
         editor.hide
       end
       evt_tree_sel_changed(get_id) do |e|
         editors.values.each{|v| v.hide}
         if editor = editors[nodes.index(e.item)]
           editor.show
-          editor_panel.sizer.recalc_sizes
+          editor_panel.sizer.layout
         end
       end
-      
       expand_all
     end
     
@@ -46,7 +49,7 @@ module Asterism
   
   class MainFrame < Wx::Frame
     def initialize(title)
-      super(nil, :title => title, :size => [640, 480])
+      super(nil, :title => title, :size => [800, 600])
       center(Wx::BOTH)
       
       # menu bar
@@ -59,34 +62,21 @@ module Asterism
       create_status_bar
       
       # project
-      models = nil
+      project = nil
       open("project1.yaml") do |fp|
-        models = YAML.load(fp)
+        project = YAML.load(fp)
       end
-
-      main_splitter_class = Class.new(Wx::SplitterWindow)
-      main_splitter_class.class_eval %Q{
-        def initialize(parent)
-          super(parent)
-          evt_splitter_sash_pos_changed(object_id) do |e|
-            e.sash_position = self.sash_position = 200
-          end
-          evt_splitter_sash_pos_changing(object_id) do |e|
-            e.sash_position = self.sash_position = 200
-          end
-        end
-      }
-      splitter = main_splitter_class.new(self)
+      
+      splitter = Wx::SplitterWindow.new(self)
       
       editor_panel = Wx::Panel.new(splitter)
-      editor_panel.background_colour = Wx::Colour.new("GREY")
       editor_panel.sizer = Wx::BoxSizer.new(Wx::VERTICAL)
       
       panel = Wx::Panel.new(splitter)
       panel.sizer = Wx::BoxSizer.new(Wx::VERTICAL)
-      panel.sizer.add(ModelSelector.new(panel, editor_panel, models), 1, Wx::EXPAND | Wx::ALL)
+      panel.sizer.add(ModelSelector.new(panel, editor_panel, project[:model_schemas]), 1, Wx::EXPAND)
       
-      splitter.split_vertically(panel, editor_panel, 200)
+      splitter.split_vertically(panel, editor_panel, 120)
       splitter.minimum_pane_size = 20
     end
   end
